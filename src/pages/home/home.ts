0b1360bac399d2ece5e720375b37033950ba0d2b
PHOTONINT\arjun_mu <arjun.murali@photoninfotech.net>
2018-02-21 19:25:25 +0530

    lifecycle event

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -22,7 +22,33 @@ export class HomePage {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad HomePage');
+    console.log('Fired only when a view is stored in memory. This event is NOT fired on entering a view that is already cached. It’s a nice place for init related tasks.');
+  }
+  ionViewWillEnter(){
+    console.log('It’s fired when entering a page, before it becomes the active one. Use it for tasks you want to do every time you enter in the view (setting event listeners, updating a table, etc.)');
+  }
+  ionViewDidEnter(){
+    console.log('Fired when entering a page, after it becomes the active page. Quite similar to the previous one.')
+  }
+  ionViewWillLeave(){
+    console.log('Fired when you leave a page, before it stops being the active one. Use it for things you need to run every time you are leaving a page (deactivate event listeners, etc.)');
+  }
+  ionViewDidLeave(){
+    console.log('Fired when you leave a page, after it stops being the active one. Similar to the previous one.')
+  }
+  ionViewWillUnload(){
+      console.log('Fired when a view is going to be completely removed (after leaving a non-cached view)');
+  }
+
+  // Nav guards
+  // It is important to highlight that Nav Guards are executed before any other lifecycle event method.
+
+  ionViewCanEnter(){
+    console.log(' Fired before entering into a view, allows you to control whether the view can be accessed or not (returning true or false)')
+  }
+
+  ionViewCanLeave(){
+    console.log(' Fired before leaving a view, allows you to control whether the view can be left or not');
   }
 
 }
