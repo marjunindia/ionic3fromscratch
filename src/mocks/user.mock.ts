87419f7cf9f760d948ac6d2ea774750e0ea452aa
arjunarjun27 <arjunarjun27@gmail.com>
2018-03-03 22:42:50 +0530

    interface and mock created

---
+++ src/mocks/user.mock.ts
@@ -0,0 +1,20 @@
+import{User} from '../models/user.interface';
+
+const userList:User[]=[
+    {
+        name:'arjun',
+        company:'PWH',
+        location:'uk',
+        url:'asdasdasd.com'
+        email:'asdsad'
+        
+    },
+    {
+        name:'aravind',
+        company:'PWHasdasd',
+        location:'us',
+        url:'asdasdasd.com'
+        email:'sadasddsas.com'
+        
+    }
+]
